4567b0048e6e7ba9872e096993ae636bcd1b42a9
Shawn <vishaanvzw@gmail.com>
2016-11-10 21:04:06 +0000

    added greeting view button callbacks

--- app.js
+++ app.js
@@ -395,7 +395,7 @@ function receivedMessage(event) {
                                   template_type: "generic",
                                   elements: [{
                                     image_url: SERVER_URL + "/assets/device_front_gadget_1_RESET.jpg",
-                                    title: ""
+                                    title: "skdj; askdjf skdjf kdjfa;ksd jfksj;l"
                                   }]
                                 }
                               }
