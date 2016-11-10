9fe641a168483033c5617bf0973be82a726e2c6f
Shawn <vishaanvzw@gmail.com>
2016-11-10 03:38:38 +0000

    reverted

--- app.js
+++ app.js
@@ -91,13 +91,13 @@ app.get('/webhook', function(req, res) {
 app.post('/webhook', function (req, res) {
   var data = req.body;
   console.log(req);
-
+/*
   callSendAPI({
       "setting_type":"greeting",
         "greeting":{
         "text":"Welcome to Gizmo Support!"
   }
-  });
+  });*/
 
   // Make sure this is a page subscription
   if (data.object == 'page') {
