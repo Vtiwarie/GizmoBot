d427122d5f09349bf04d89126ebc1371315c4019
Shawn <vishaanvzw@gmail.com>
2016-11-10 20:47:38 +0000

    added greeting view button callbacks

--- app.js
+++ app.js
@@ -395,7 +395,6 @@ function receivedMessage(event) {
                                   template_type: "generic",
                                   elements: [{
                                     image_url: SERVER_URL + "/assets/device_front_gadget_1_RESET.jpg",
-                                    title: "Quickly press the End/Back button 4 times."
                                   }]
                                 }
                               }
