5ec564130c50bbf66f079ed0ae045ff50cca9bd0
Shawn <vishaanvzw@gmail.com>
2016-11-11 17:53:12 +0000

    changed flow text

--- app.js
+++ app.js
@@ -454,7 +454,7 @@ function receivedMessage(event) {
                     }],
                   }, {
                     title: "Unable to Link, Linking Replace-ment Gizmo, Changing Primary CareGiver, Adding Caregivers",
-                    image_url: SERVER_URL + "/assets/Registration_Icon.png",
+                    image_url: SERVER_URL + "/assets/Register_Icon.png",
                     buttons: [{
                       type: "web_url",
                       url: "https://www.oculus.com/en-us/touch/",
